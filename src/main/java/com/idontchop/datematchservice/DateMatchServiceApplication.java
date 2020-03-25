ff16557abf0ab8871db041aea87f1fb937bb6ad8
Nate D <nate@idontchop.com>
2020-03-24 21:39:20 -0700

    Initial Commit

---
+++ src/main/java/com/idontchop/datematchservice/DateMatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.idontchop.datematchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateMatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateMatchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idontchop/datematchservice/DateMatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idontchop.datematchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateMatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
