91a18f4467e717427d39f30390a69b0dc4355566
Nate <nate@idontchop.com>
2021-04-30 11:54:40 -0700

    host net

--- jenkins/deployDockerImage.sh
+++ jenkins/deployDockerImage.sh
@@ -1,3 +1,3 @@
 docker stop $IMAGE
 docker rm $IMAGE
-docker run -d --rm --name $IMAGE -p 8931:8931 192.168.1.55:5000/$IMAGE
+docker run -d --rm --name $IMAGE -p 8931:8931 --net=host 192.168.1.55:5000/$IMAGE
